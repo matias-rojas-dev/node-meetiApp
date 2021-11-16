a0878f98b69dcafcee39863ded1f8e7339ecbd1f
Matias Rojas <maigrojas.dev@gmail.com>
2021-11-16 18:14:17 -0300

    Create a create account page

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts');
 const path = require('path')
 require('dotenv').config({ path: 'variables.env' })
 const router = require('./routes')
 
 
 const app = express()
+app.use(expressLayouts)
+app.set('view engine', 'ejs') // haenable EJS as template engine
+app.set('views', path.join(__dirname, './views')) // views location
 
 app.use('/', router())
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, './views'))
+app.use(expressLayouts)
 
-app.use(express.static('public'))
+
+app.use(express.static('public')) //static files
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running')
